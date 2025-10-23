4fdfe70760489bfee23b657b7be62768ba9b52d8
sjohnston82 <sjohnston82@users.noreply.github.com>
2025-10-22 21:58:04 -0400

    adding title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -24,7 +24,7 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "TanStack Start Starter",
+        title: "Recipe Roundup",
       },
     ],
     links: [
